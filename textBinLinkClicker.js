8585f6ff0ddf294d028103d4ea4ad94f45a8c64b
Jay <42314425+JethaLal420@users.noreply.github.com>
2021-12-22 19:16:30 +0530

    Solved ClipBoard not working
    Uncaught (in promise) DOMException: Document is not focused Error solved.

--- textBinLinkClicker.js
+++ textBinLinkClicker.js
@@ -29,8 +29,10 @@
             }
             window.open(url, '_self')
         }, 3000)
-    } else {
-        var inputField = document.getElementsByTagName('input')[0]
+    }
+    /*else {
+        setTimeout(() => {
+            var inputField = document.getElementsByTagName('input')[0]
         var btn = document.getElementsByTagName('button')[1]
 
         navigator.clipboard.readText().then(clipText => {
@@ -42,6 +44,39 @@
                 window.open(link, '_self')
             }, 3000)
         })
+        }, 2000)
+    }*/
+
+    // Uncaught (in promise) DOMException: Document is not focused Error solved from https://stackoverflow.com/a/70386674
+    else {
+        function readClipboardFromDevTools() {
+            return new Promise((resolve, reject) => {
+                const _asyncCopyFn = (async () => {
+                    try {
+                        const value = await navigator.clipboard.readText();
+                        console.log(`${value} is read!`);
+                        resolve(value);
+                    } catch (e) {
+                        reject(e);
+                    }
+                    window.removeEventListener("focus", _asyncCopyFn);
+                });
+
+                window.addEventListener("focus", _asyncCopyFn);
+            });
+        }
+        var inputField = document.getElementsByTagName('input')[0]
+        var btn = document.getElementsByTagName('button')[1]
+        readClipboardFromDevTools().then((clipText) => {
+            console.log(clipText)
+            inputField.value = clipText
+            btn.click()
+            setTimeout(() => {
+                var link = document.getElementsByName('pasteText')[0].value
+                link = link.split('-')[0]
+                window.open(link, '_self')
+            }, 3000)
+        });
     }
 
 })();
